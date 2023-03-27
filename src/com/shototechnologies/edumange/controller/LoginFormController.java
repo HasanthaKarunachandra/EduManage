83acdeb244598c39112c5625b1c6eebeacbb1097
Hasantha Karunachandra <32540627+HasanthaKarunachandra@users.noreply.github.com>
2023-03-27 14:13:45 +0530

    Login

--- src/com/shototechnologies/edumange/controller/LoginFormController.java
+++ src/com/shototechnologies/edumange/controller/LoginFormController.java
@@ -1,4 +1,54 @@
 package com.shototechnologies.edumange.controller;
 
+import com.shototechnologies.edumange.db.Database;
+import com.shototechnologies.edumange.model.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+        String email=txtEmail.getText().toLowerCase();
+        String password=txtPassword.getText().trim();
+
+        Optional<User>selectedUser= Database.userTable.stream().filter(e->e.getEmail().equals(email)).findFirst();
+        if (selectedUser.isPresent()){
+            if(selectedUser.get().getPassword().equals(password)){
+                System.out.println(selectedUser.get().toString());
+            }else{
+                new Alert(Alert.AlertType.ERROR,
+                        "Wrong Password!").show();
+            }
+        }else{
+            new Alert(Alert.AlertType.WARNING,
+                    String.format("User not found (%s)",email)).show();
+        }
+    }
+
+    public void forgotPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignupForm");
+    }
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
+
+    }
 }
--- src/com/shototechnologies/edumange/controller/SignupFormController.java
+++ src/com/shototechnologies/edumange/controller/SignupFormController.java
@@ -1,4 +1,46 @@
 package com.shototechnologies.edumange.controller;
 
+import com.shototechnologies.edumange.db.Database;
+import com.shototechnologies.edumange.model.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SignupFormController {
+    public AnchorPane context;
+    public TextField txtFirstname;
+    public PasswordField txtPassword;
+    public TextField txtLastname;
+    public TextField txtEmail;
+
+    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
+        String email = txtEmail.getText().toLowerCase();
+        String firstname=txtFirstname.getText();
+        String lastName=txtLastname.getText();
+        String password= txtPassword.getText().trim();
+        Database.userTable.add(
+                new User(firstname,lastName,email,password)
+        );
+        new Alert(Alert.AlertType.INFORMATION,"Welcome").show();
+        setUi("LoginForm");
+
+    }
+
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
+    }
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
+
+    }
 }
