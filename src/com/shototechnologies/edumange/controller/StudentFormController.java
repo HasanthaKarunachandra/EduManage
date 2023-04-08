8b1b9dd55f496ff5795e40a2a4d245fac3b29227
Hasantha Karunachandra <32540627+HasanthaKarunachandra@users.noreply.github.com>
2023-04-09 00:08:18 +0530

    Student Form added.

---
+++ src/com/shototechnologies/edumange/controller/StudentFormController.java
@@ -0,0 +1,61 @@
+package com.shototechnologies.edumange.controller;
+
+import com.shototechnologies.edumange.db.Database;
+import com.shototechnologies.edumange.model.Student;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.time.ZoneId;
+import java.util.Date;
+
+public class StudentFormController {
+    public AnchorPane context;
+    public TextField txtId;
+    public TextField txtName;
+    public DatePicker txtDob;
+    public TextField txtAddress;
+
+    public void initialize(){
+        setStudentId();
+    }
+
+    private void setStudentId() {
+        if (!Database.studentTable.isEmpty()){
+            Student lastStudent = Database.studentTable.get(
+                    Database.studentTable.size()-1
+            );
+            String lastId= lastStudent.getStudentId();
+            String splitData[]=lastId.split("-");
+            String lastIdIntegerNumberAsAString=splitData[1];
+            int lastIntegerIdAsInt=Integer.parseInt(lastIdIntegerNumberAsAString);
+            lastIntegerIdAsInt++;
+            String generatedStudentId="S-"+lastIntegerIdAsInt;
+            txtId.setText(generatedStudentId);
+
+        }else {
+            txtId.setText("S-1");
+        }
+    }
+
+    public void saveOnAction(ActionEvent actionEvent) {
+        Student student= new Student(
+             txtId.getText(),
+             txtName.getText(),
+             Date.from(txtDob.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()),
+             txtAddress.getText()
+        );
+        System.out.println(student);
+        Database.studentTable.add(student);
+        setStudentId();
+        clear();
+        new Alert(Alert.AlertType.INFORMATION,"Student saved!").show();
+    }
+    private void  clear(){
+        txtName.clear();
+        txtDob.setValue(null);
+        txtAddress.clear();
+    }
+}
