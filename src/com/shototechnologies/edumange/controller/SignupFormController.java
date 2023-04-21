65ed48d3c05fee670a6c560e6c34ec1c510ab52b
Hasantha Karunachandra <32540627+HasanthaKarunachandra@users.noreply.github.com>
2023-04-21 18:57:34 +0530

    added JDBC and Singupform connected to MySql database.

--- src/com/shototechnologies/edumange/controller/SignupFormController.java
+++ src/com/shototechnologies/edumange/controller/SignupFormController.java
@@ -13,6 +13,10 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SignupFormController {
     public AnchorPane context;
@@ -21,18 +25,7 @@ public class SignupFormController {
     public TextField txtLastname;
     public TextField txtEmail;
 
-    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
-        String email = txtEmail.getText().toLowerCase();
-        String firstname=txtFirstname.getText();
-        String lastName=txtLastname.getText();
-        String password= new PasswordManager().encrypt(txtPassword.getText().trim());
-        Database.userTable.add(
-                new User(firstname,lastName,email,password)
-        );
-        new Alert(Alert.AlertType.INFORMATION,"Welcome").show();
-        setUi("LoginForm");
 
-    }
 
     public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
         setUi("LoginForm");
@@ -43,5 +36,40 @@ public class SignupFormController {
                 new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
         stage.centerOnScreen();
 
+    }
+    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
+        String email = txtEmail.getText().toLowerCase();
+        String firstname=txtFirstname.getText();
+        String lastName=txtLastname.getText();
+
+
+        String password= new PasswordManager().encrypt(txtPassword.getText().trim());
+
+
+        User createUser = new User(firstname, lastName, email, password);
+        try{
+            boolean isSaved = signup(createUser);
+
+            if(isSaved){
+                new Alert(Alert.AlertType.INFORMATION,"Welcome").show();
+                setUi("LoginForm");
+            }else{
+                new Alert(Alert.AlertType.WARNING,"Try Again!").show();
+            }
+
+        }catch(SQLException | ClassNotFoundException e1){
+            new Alert(Alert.AlertType.ERROR,e1.toString()).show();
+        }
+
+    }
+
+    private boolean signup(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection=
+        DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","user","password");
+        String sql="INSERT INTO User VALUES ('"+user.getEmail()+"','"+user.getFirstName()+"','"+user.getLastName()+"','"+user.getPassword()+"')";
+
+        Statement statement=connection.createStatement();
+        return statement.executeUpdate(sql)>0;
     }
 }
